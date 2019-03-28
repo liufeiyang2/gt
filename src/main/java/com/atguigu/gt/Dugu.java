7d4de88c9f0edefaed9fb3fb600823efaab9f195
liufeiyang2 <1184277345@qq.com>
2019-03-28 10:09:05 +0800

    修改

--- src/main/java/com/atguigu/gt/Dugu.java
+++ src/main/java/com/atguigu/gt/Dugu.java
@@ -9,13 +9,7 @@ public class Dugu {
         System.out.println("我是会/////");
         System.out.println("我是会/////");
         System.out.println("我是会/////");
-        System.out.println("我是会/////");
-        System.out.println("我是会/////");
-        System.out.println("我是会/////");
-        System.out.println("我是会/////");
-        System.out.println("我是会/////");
-        System.out.println("我是会/////");
-        System.out.println("我是会/////");
-        System.out.println("我是会/////");
+        System.out.println("我是会/~~~~~~~~");
+
     }
 }
